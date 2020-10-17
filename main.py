34a01f6b16043c59d255e3f16f0694540f80ea8c
sleduap <sleduap@gmail.com>
2020-10-17 15:21:58 +0545

    Version 1.0.0

---
+++ main.py
@@ -0,0 +1,38 @@
+from urllib import request
+from sys import stdout
+import time
+
+
+def file_to_download():
+    with request.urlopen("https://nl01.cutepup.club/735e03955b1d878a86b44/Imagine%20Dragons%20-%20Believer.mp3") as \
+          handler:
+
+        return handler.read()
+
+
+def download_status(func):
+
+    def inner_function(music, filename, extension):
+
+        if music:
+
+            return func(music, filename, extension)
+
+        print("No files to download")
+    return inner_function
+
+
+@download_status
+def save_to_local_disk(music, filename, extension):
+    with open(filename+extension, "wb") as wri:
+        print("Starting Downloads")
+
+        stdout.flush()
+        time.sleep(1)
+        print(wri.write(music))
+        print("Download Completed")
+
+
+file_name = "Believer"
+music_file = file_to_download()
+save_to_local_disk(music_file, file_name, ".mp3")
