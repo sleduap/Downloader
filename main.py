386c8aa8162c1346d863f378197b65909fc3a874
sleduap <sleduap@gmail.com>
2020-10-18 11:39:41 +0545

    Version 1.0.1

--- main.py
+++ main.py
@@ -4,29 +4,26 @@ import time
 
 
 def file_to_download():
+    print("Download Started Wait for it to complete")
+    print("="*8)
     with request.urlopen("https://nl01.cutepup.club/735e03955b1d878a86b44/Imagine%20Dragons%20-%20Believer.mp3") as \
-          handler:
-
+            handler:
         return handler.read()
 
 
 def download_status(func):
-
     def inner_function(music, filename, extension):
-
         if music:
-
             return func(music, filename, extension)
 
         print("No files to download")
+
     return inner_function
 
 
 @download_status
 def save_to_local_disk(music, filename, extension):
-    with open(filename+extension, "wb") as wri:
-        print("Starting Downloads")
-
+    with open(filename + extension, "wb") as wri:
         stdout.flush()
         time.sleep(1)
         print(wri.write(music))
